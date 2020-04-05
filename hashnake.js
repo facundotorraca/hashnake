b67220f4896b27d14c8e3f7d993e5d56235fbf05
facutorraca <ftorraca@fi.uba.ar>
2020-04-05 16:31:01 -0300

    first attempt to round screen size

--- hashnake.js
+++ hashnake.js
@@ -1,30 +1,62 @@
+const XL_SCREEN = 1200;
+const LG_SCREEN = 992;
+const MD_SCREEN = 768;
+
+const XL_SNAKE_BOX = 20;
+const LG_SNAKE_BOX = 16;
+const MD_SNAKE_BOX = 14;
+const SM_SNAKE_BOX = 12;
+
+let screenSize = undefined;
+let snakeBoxSize = undefined;
+
 $(document).ready(() => {
-    let screenSize = _getScreenSize();
-    screenSize = _roundUpScreenSize(screenSize);
+    roundScreenSize();
+    screenSize = getScreenSize();
+    snakeBoxSize = getSnakeBoxSize();
+
+    window.addEventListener("orientationchange", () => handleOrientationChange());
 });
 
-function _getScreenSize() {
-    let screen = _getScreen();
+function handleOrientationChange() {
+    location.reload();
+}
+
+function getSnakeBoxSize() {
+    // Values from Bootstrap
+    if (window.innerWidth >= XL_SCREEN)
+        return XL_SNAKE_BOX;
+    if (window.innerWidth >= LG_SCREEN)
+        return LG_SNAKE_BOX;
+    if (window.innerWidth >= MD_SCREEN)
+        return MD_SNAKE_BOX;
+    //if (window.innerWidth < SM_SCREEN)
+    return SM_SNAKE_BOX;
+}
+
+function getScreenSize() {
+    let screen = getScreen();
     let height = screen.getBoundingClientRect().height;
     let width = screen.getBoundingClientRect().width;
     return {height: height, width: width};
 }
 
-function _roundUpScreenSize(currentScreenSize) {
-    let $screen = $('#screen');
-    let heightCeil = Math.ceil(currentScreenSize.height) + 'px';
-    let widthCeil = Math.ceil(currentScreenSize.width) + 'px';
+function roundScreenSize() {
+    let currentScreenSize = getScreenSize();
 
-    $screen.css('width', widthCeil);
-    $screen.css('height',heightCeil);
-    return _getScreenSize();
+    let newHeight = Math.floor(currentScreenSize.height);
+    let newWidth = Math.floor(currentScreenSize.width);
+
+    $screen = $('#screen');
+    $screen.css('width', newWidth);
+    $screen.css('height',newHeight);
 }
 
-function _getCanvasContext() {
+function getCanvasContext() {
     return $('#screen')[0].getContext('2d');
 }
 
-function _getScreen() {
+function getScreen() {
     /*dont know why query selector dont work*/
     return document.getElementById('screen');
 }

428a8c1316dee75b57d04f6be720af9a09c17884
facutorraca <ftorraca@fi.uba.ar>
2020-04-05 15:57:48 -0300

    round up canvas working

--- hashnake.js
+++ hashnake.js
@@ -1,2 +1,30 @@
 $(document).ready(() => {
+    let screenSize = _getScreenSize();
+    screenSize = _roundUpScreenSize(screenSize);
 });
+
+function _getScreenSize() {
+    let screen = _getScreen();
+    let height = screen.getBoundingClientRect().height;
+    let width = screen.getBoundingClientRect().width;
+    return {height: height, width: width};
+}
+
+function _roundUpScreenSize(currentScreenSize) {
+    let $screen = $('#screen');
+    let heightCeil = Math.ceil(currentScreenSize.height) + 'px';
+    let widthCeil = Math.ceil(currentScreenSize.width) + 'px';
+
+    $screen.css('width', widthCeil);
+    $screen.css('height',heightCeil);
+    return _getScreenSize();
+}
+
+function _getCanvasContext() {
+    return $('#screen')[0].getContext('2d');
+}
+
+function _getScreen() {
+    /*dont know why query selector dont work*/
+    return document.getElementById('screen');
+}

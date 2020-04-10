0e40b2bf78d66636bd7d382fe2caac6e0fe0f513
facutorraca <ftorraca@fi.uba.ar>
2020-04-09 21:18:03 -0300

    poster code refactored

--- poster.js
+++ poster.js
@@ -1,5 +1,8 @@
-import {AnimatedH, AnimatedA, AnimatedS, AnimatedN, AnimatedK, AnimatedE, AnimatedG, AnimatedM, AnimatedO, AnimatedV, AnimatedR, UPDATE_TIME} from './letter.js';
+import {AnimatedH, AnimatedA, AnimatedS, AnimatedN, AnimatedK, AnimatedE,
+        AnimatedG, AnimatedM, AnimatedO, AnimatedV, AnimatedR,
+        UPDATE_TIME, LETTER_WIDTH, LETTER_HEIGHT} from './letter.js';
 
+/*------AUXILIARY-LOADING_FUNCTIONS-------*/
 function _loadIntroLandscape(poster, cellSide, posterHeight) {
     poster.push(new AnimatedH(cellSide, 1,  posterHeight));
     poster.push(new AnimatedA(cellSide, 6,  posterHeight));
@@ -16,12 +19,11 @@ function _loadIntroPortrait(poster, cellSide, posterHeight, xOffset) {
     poster.push(new AnimatedA(cellSide, 6 + xOffset,  posterHeight));
     poster.push(new AnimatedS(cellSide, 11 + xOffset, posterHeight));
     poster.push(new AnimatedH(cellSide, 16 + xOffset, posterHeight));
-    poster.push(new AnimatedN(cellSide, 1 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedA(cellSide, 6 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedK(cellSide, 11 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedE(cellSide, 16 + xOffset, posterHeight + 8));
+    poster.push(new AnimatedN(cellSide, 1 + xOffset, posterHeight + LETTER_HEIGHT + 1));
+    poster.push(new AnimatedA(cellSide, 6 + xOffset, posterHeight + LETTER_HEIGHT +1));
+    poster.push(new AnimatedK(cellSide, 11 + xOffset, posterHeight + LETTER_HEIGHT + 1));
+    poster.push(new AnimatedE(cellSide, 16 + xOffset, posterHeight + LETTER_HEIGHT +1));
 }
-
 
 function _loadGameOverLandscape(poster, cellSide, posterHeight) {
     poster.push(new AnimatedG(cellSide, 1,  posterHeight));
@@ -39,12 +41,13 @@ function _loadGameOverPortrait(poster, cellSide, posterHeight, xOffset) {
     poster.push(new AnimatedA(cellSide, 6 + xOffset,  posterHeight));
     poster.push(new AnimatedM(cellSide, 11 + xOffset, posterHeight));
     poster.push(new AnimatedE(cellSide, 16 + xOffset, posterHeight));
-    poster.push(new AnimatedO(cellSide, 1 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedV(cellSide, 6 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedE(cellSide, 11 + xOffset, posterHeight + 8));
-    poster.push(new AnimatedR(cellSide, 16 + xOffset, posterHeight + 8));
+    poster.push(new AnimatedO(cellSide, 1 + xOffset, posterHeight + LETTER_HEIGHT + 1));
+    poster.push(new AnimatedV(cellSide, 6 + xOffset, posterHeight + LETTER_HEIGHT + 1));
+    poster.push(new AnimatedE(cellSide, 11 + xOffset, posterHeight + LETTER_HEIGHT + 1));
+    poster.push(new AnimatedR(cellSide, 16 + xOffset, posterHeight + LETTER_HEIGHT + 1));
 }
 
+/*----------AUXILIARY-FUNCTIONS-----------*/
 function _getPosterDrawingTime(poster) {
     let maxDrawingTime = 0;
     for (const letter of poster) {
@@ -53,39 +56,56 @@ function _getPosterDrawingTime(poster) {
     }
     //Add a piece of time to make the intro message
     //appear correctly
-    return maxDrawingTime + UPDATE_TIME;
+    return maxDrawingTime + 2 * UPDATE_TIME;
 }
 
 function _getScreenOrientation(screenSize) {
     return (screenSize.height > screenSize.width) ? 'portrait' : 'landscape';
 }
 
-function Poster(cellSide, posterHeight, screenSize, portraitLoadingFunction, landscapeLoadingFunction) {
-    this.poster = [];
-
-    if (_getScreenOrientation(screenSize) === 'portrait') {
-        //It only works for 8 letters posters (19 is half word -> 4*4(sizeLetter) + 3 (spaces)
-        let lineCells = screenSize.width / cellSide;
-        let xOffset = Math.floor((lineCells % 19)/2 - 1);
-        portraitLoadingFunction(this.poster, cellSide, posterHeight, xOffset);
-    } else {
-        landscapeLoadingFunction(this.poster, cellSide, posterHeight);
-    }
-
-    this.drawingTime = _getPosterDrawingTime(this.poster);
+/*-----------------POSTER-----------------*/
+function Poster(totalLetters) {
+    this._poster = [];
+    this._totalLetters = totalLetters;
 
     this.draw = function (screenContext, color) {
-        for (const letter of this.poster)
+        let drawingTime = _getPosterDrawingTime(this._poster);
+
+        for (const letter of this._poster)
             letter.draw(screenContext, color);
         //Dont know if is the best way. I wait some suggestions to correct
-        return new Promise(resolve => setTimeout(resolve, this.drawingTime));
+        return new Promise(resolve => setTimeout(resolve, drawingTime));
     }
 }
 
 export function Intro(cellSide, posterHeight, screenSize) {
-    Poster.call(this, cellSide, posterHeight, screenSize, _loadIntroPortrait, _loadIntroLandscape);
+    Poster.call(this, 8);
+
+    let screenOrientation = _getScreenOrientation(screenSize);
+
+    if (screenOrientation === 'portrait') {
+        let lineCells = screenSize.width / cellSide;
+        let portraitLineLetters = Math.floor(this._totalLetters / 2);
+        let lineLength = (portraitLineLetters * LETTER_WIDTH) + 3;/*spaces*/
+        let xOffset = Math.floor((lineCells % lineLength)/2 - 1);
+        _loadIntroPortrait(this._poster, cellSide, posterHeight, xOffset);
+    } else {
+        _loadIntroLandscape(this._poster, cellSide, posterHeight, xOffset);
+    }
 }
 
 export function GameOver(cellSide, posterHeight, screenSize) {
-    Poster.call(this, cellSide, posterHeight, screenSize, _loadGameOverPortrait, _loadGameOverLandscape);
+    Poster.call(this, 8);
+
+    let screenOrientation = _getScreenOrientation(screenSize);
+
+    if (screenOrientation === 'portrait') {
+        let lineCells = screenSize.width / cellSide;
+        let portraitLineLetters = Math.floor(this._totalLetters / 2);
+        let lineLength = (portraitLineLetters * LETTER_WIDTH) + 3;/*spaces*/
+        let xOffset = Math.floor((lineCells % lineLength)/2 - 1);
+        _loadGameOverPortrait(this._poster, cellSide, posterHeight, xOffset);
+    } else {
+        _loadGameOverLandscape(this._poster, cellSide, posterHeight, xOffset);
+    }
 }

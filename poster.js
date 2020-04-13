1f617625134aece373579fca2d9cfef75d622ad8
facutorraca <ftorraca@gmail.com>
2020-04-12 23:17:48 -0300

    fix centering posters on landscape

--- poster.js
+++ poster.js
@@ -4,15 +4,15 @@ import {AnimatedH, AnimatedA, AnimatedS, AnimatedN, AnimatedK, AnimatedE,
         UPDATE_TIME, LETTER_WIDTH, LETTER_HEIGHT} from './letter.js';
 
 /*------AUXILIARY-LOADING_FUNCTIONS-------*/
-function _loadIntroLandscape(poster, cellSide, posterHeight) {
-    poster.push(new AnimatedH(cellSide, 1,  posterHeight));
-    poster.push(new AnimatedA(cellSide, 6,  posterHeight));
-    poster.push(new AnimatedS(cellSide, 11, posterHeight));
-    poster.push(new AnimatedH(cellSide, 16, posterHeight));
-    poster.push(new AnimatedN(cellSide, 21, posterHeight));
-    poster.push(new AnimatedA(cellSide, 26, posterHeight));
-    poster.push(new AnimatedK(cellSide, 31, posterHeight));
-    poster.push(new AnimatedE(cellSide, 36, posterHeight));
+function _loadIntroLandscape(poster, cellSide, posterHeight, xOffset) {
+    poster.push(new AnimatedH(cellSide, 1 + xOffset,  posterHeight));
+    poster.push(new AnimatedA(cellSide, 6 + xOffset,  posterHeight));
+    poster.push(new AnimatedS(cellSide, 11 + xOffset, posterHeight));
+    poster.push(new AnimatedH(cellSide, 16 + xOffset, posterHeight));
+    poster.push(new AnimatedN(cellSide, 21 + xOffset, posterHeight));
+    poster.push(new AnimatedA(cellSide, 26 + xOffset, posterHeight));
+    poster.push(new AnimatedK(cellSide, 31 + xOffset, posterHeight));
+    poster.push(new AnimatedE(cellSide, 36 + xOffset, posterHeight));
 }
 
 function _loadIntroPortrait(poster, cellSide, posterHeight, xOffset) {
@@ -26,15 +26,15 @@ function _loadIntroPortrait(poster, cellSide, posterHeight, xOffset) {
     poster.push(new AnimatedE(cellSide, 16 + xOffset, posterHeight + LETTER_HEIGHT +1));
 }
 
-function _loadGameOverLandscape(poster, cellSide, posterHeight) {
-    poster.push(new AnimatedG(cellSide, 1,  posterHeight));
-    poster.push(new AnimatedA(cellSide, 6,  posterHeight));
-    poster.push(new AnimatedM(cellSide, 11, posterHeight));
-    poster.push(new AnimatedE(cellSide, 16, posterHeight));
-    poster.push(new AnimatedO(cellSide, 21, posterHeight));
-    poster.push(new AnimatedV(cellSide, 26, posterHeight));
-    poster.push(new AnimatedE(cellSide, 31, posterHeight));
-    poster.push(new AnimatedR(cellSide, 36, posterHeight));
+function _loadGameOverLandscape(poster, cellSide, posterHeight, xOffset) {
+    poster.push(new AnimatedG(cellSide, 1 + xOffset,  posterHeight));
+    poster.push(new AnimatedA(cellSide, 6 + xOffset,  posterHeight));
+    poster.push(new AnimatedM(cellSide, 11 + xOffset, posterHeight));
+    poster.push(new AnimatedE(cellSide, 16 + xOffset, posterHeight));
+    poster.push(new AnimatedO(cellSide, 21 + xOffset, posterHeight));
+    poster.push(new AnimatedV(cellSide, 26 + xOffset, posterHeight));
+    poster.push(new AnimatedE(cellSide, 31 + xOffset, posterHeight));
+    poster.push(new AnimatedR(cellSide, 36 + xOffset, posterHeight));
 }
 
 function _loadGameOverPortrait(poster, cellSide, posterHeight, xOffset) {
@@ -64,6 +64,25 @@ function _getScreenOrientation(screenSize) {
     return (screenSize.height > screenSize.width) ? 'portrait' : 'landscape';
 }
 
+function _getXOffsetToCenter(totalLetters, screenSize, cellSide, orientation) {
+    let xOffset = undefined;
+    let lineCells = screenSize.width / cellSide;
+
+    if (orientation === 'landscape') {
+        let spaces = totalLetters - 1;
+        let posterLength = (totalLetters * LETTER_WIDTH) + spaces;
+        xOffset = Math.floor((lineCells % posterLength)/2 - 1);
+    }
+
+    if (orientation === 'portrait') {
+        let portraitLineLetters = Math.floor(totalLetters / 2);
+        let spaces = portraitLineLetters - 1;
+        let posterLength = (portraitLineLetters * LETTER_WIDTH) + spaces;
+        xOffset = Math.floor((lineCells % posterLength)/2 - 1);
+    }
+
+    return xOffset;
+}
 /*-----------------POSTER-----------------*/
 function Poster(totalLetters) {
     this._poster = [];
@@ -83,30 +102,22 @@ export function Intro(cellSide, posterHeight, screenSize) {
     Poster.call(this, 8);
 
     let screenOrientation = _getScreenOrientation(screenSize);
+    let xOffset = _getXOffsetToCenter(this._totalLetters, screenSize, cellSide, screenOrientation);
 
-    if (screenOrientation === 'portrait') {
-        let lineCells = screenSize.width / cellSide;
-        let portraitLineLetters = Math.floor(this._totalLetters / 2);
-        let lineLength = (portraitLineLetters * LETTER_WIDTH) + 3;/*spaces*/
-        let xOffset = Math.floor((lineCells % lineLength)/2 - 1);
+    if (screenOrientation === 'portrait')
         _loadIntroPortrait(this._poster, cellSide, posterHeight, xOffset);
-    } else {
-        _loadIntroLandscape(this._poster, cellSide, posterHeight);
-    }
+    else
+        _loadIntroLandscape(this._poster, cellSide, posterHeight, xOffset);
 }
 
 export function GameOver(cellSide, posterHeight, screenSize) {
     Poster.call(this, 8);
 
     let screenOrientation = _getScreenOrientation(screenSize);
+    let xOffset = _getXOffsetToCenter(this._totalLetters, screenSize, cellSide, screenOrientation);
 
-    if (screenOrientation === 'portrait') {
-        let lineCells = screenSize.width / cellSide;
-        let portraitLineLetters = Math.floor(this._totalLetters / 2);
-        let lineLength = (portraitLineLetters * LETTER_WIDTH) + 3;/*spaces*/
-        let xOffset = Math.floor((lineCells % lineLength)/2 - 1);
+    if (screenOrientation === 'portrait')
         _loadGameOverPortrait(this._poster, cellSide, posterHeight, xOffset);
-    } else {
-        _loadGameOverLandscape(this._poster, cellSide, posterHeight);
-    }
+    else
+        _loadGameOverLandscape(this._poster, cellSide, posterHeight, xOffset);
 }
